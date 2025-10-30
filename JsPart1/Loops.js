d627b2967cfa9575b6cfca5466c560dad9e7723a
Abhishek3777 <kr7abhishek04@gmail.com>
2025-10-30 19:13:46 +0530

    added traversing of array of objects vice versa and json to object conversion

--- JsPart1/Loops.js
+++ JsPart1/Loops.js
@@ -1,6 +1,6 @@
 //1.  For Loop
 for (let i = 0; i < 5; i++) {
-    console.log(i);
+    // console.log(i);
 }
 
 // 2. For of -> Arrays, strings, maps, set
@@ -8,12 +8,12 @@ const arr = ['apple', 'banana', 'mango'];
 const obj = { name: 'Dexter', age: 22 };
 
 for (let fruit of arr) {
-    console.log(fruit);
+    // console.log(fruit);
 }
 
 //3.  For in -> Objects, arrays
 for (let key in obj) {
-    console.log(key, '', obj[key]);
+    // console.log(key, '', obj[key]);
 }
 
 // 4. For each -> Arrays only
@@ -24,3 +24,51 @@ arr.forEach((fruit, i) => console.log(fruit, i));
 // 🟦 for...of → “of values” (arrays, strings)
 // 🟨 for...in → “in keys” (objects)
 // 🟧 forEach → “array method” (no break, no async)
+
+// looping thru an array of objects
+
+// 1. 🧩 1. Array of Objects
+const users = [
+    { id: 1, name: "Abhi", age: 22 },
+    { id: 2, name: "Riya", age: 24 },
+    { id: 3, name: "Aman", age: 20 },
+];
+// using traditional for loop
+for (let i = 0; i < users.length; i++) {
+    // console.log(users[i].id, users[i].name, users[i].age);
+}
+// using forEach loop
+// users.forEach(user =>
+//     // console.log(user.id, user.name, user.age)
+// )
+
+// 2. Object of objects
+const users1 = {
+    user1: { name: "Abhi", age: 22 },
+    user2: { name: "Riya", age: 24 },
+    user3: { name: "Aman", age: 20 },
+};
+
+for (const [key, value] of Object.entries(users)) {
+    console.log(key, value.name, value.age);
+}
+
+// JSON text to object 
+// case 1 with array of objects
+
+const json = '[{"id":1,"name":"Abhi"},{"id":2,"name":"Riya"}]';
+
+const arr1 = JSON.parse(json);
+
+for (let user of arr1) {
+    console.log(user.id, user.name);
+}
+//  Case 2 can be json is in form of objects of objects
+
+// Sending data to an api
+
+// JSON.stringify() -> Object -> JSON object
+
+const obj1 = {name : 'Dexter', age: 21};
+const jsonString = JSON.stringify(obj1);
+console.log(jsonString);
