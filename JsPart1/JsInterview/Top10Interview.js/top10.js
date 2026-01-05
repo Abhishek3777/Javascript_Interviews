06e9dbf9f91743119213bb0615fc5b95864a4e19
Abhishek3777 <kr7abhishek04@gmail.com>
2026-01-05 16:04:02 +0530

    added more ques

--- JsPart1/JsInterview/Top10Interview.js/top10.js
+++ JsPart1/JsInterview/Top10Interview.js/top10.js
@@ -33,4 +33,53 @@ console.log(ans);
 let uniqueArr = [...new Set(dup)];
 console.log(uniqueArr);
 
+// Reverse a string in js
+// i) using built in method
+let str = "hello world";
+let revStr = str.split("").reverse().join("");
+console.log(revStr);
+
+// ii) not using built in method
+let str1 = "";
+
+for(let i = str.length - 1; i >= 0; i--){
+    str1 += str[i];
+}
+console.log(str1);
+
+// Ques 3 Find the largest number in an array
+let arr2 = [1,2,3,56];
+let maxi = 0;
+for(let i = 0; i < arr2.length; i++){
+    if(arr2[i] > maxi)
+        maxi = arr2[i];
+}
+console.log(maxi);
+
+// using spread operator or math.max
+function findLargest(arr){
+    return Math.max(...arr);
+}
+console.log(findLargest([2, 99, 566, 34]));
+
+// Q4 Find frequency of elements in an array
+
+function frequency(arr){
+    const freq = {};
+    for(let i = 0; i < arr.length; i++){
+        if(freq[arr[i]]){
+            freq[arr[i]] += 1;
+        }
+        else{
+            freq[arr[i]] = 1;
+        }
+    }
+    console.log((freq));
+    
+}
+
+console.log(frequency([1,1,2,2,2,3,3,3,4]));
+
+
+
 
